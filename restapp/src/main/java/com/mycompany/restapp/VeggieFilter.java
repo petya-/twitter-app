5cc76e742f3e9741a3ac149ad1eb61138ba84ff8
Eric York <erickofiyork@gmail.com>
2019-03-11 01:39:45 +0100

    Cleaned up by removing old unused code.

--- restapp/src/main/java/com/mycompany/restapp/VeggieFilter.java
+++ restapp/src/main/java/com/mycompany/restapp/VeggieFilter.java
@@ -13,7 +13,6 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
@@ -62,63 +61,6 @@ public class VeggieFilter extends HttpServlet {
         filters.put("all", allVegetables);
 
         gson = new Gson();
-    }
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet VeggieFilter</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet VeggieFilter at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-
-            //1: Take a "text/plain" Content-Type body (the header not required, but if included, needs to be text/plain)
-            //TODO 2: Respond with JSON format text?
-            //TODO 3: The Response header should also state that the response type is JSON
-            //TODO 4: status codes
-            //TODO 5: Have a method that returns the names used for each available filter.
-            //Check 'Content-Type' header
-            String contentType = request.getContentType();
-            if (contentType != null && contentType.contains("text/plain") == false) {
-                //TODO: Abort mission!
-            }
-
-            Scanner scanner = new Scanner(request.getInputStream(), request.getCharacterEncoding());
-
-            String s;
-            while (scanner.hasNext()) {
-                s = scanner.next();
-
-                //clean up the end of tokens
-                while (Character.isLetter(s.charAt(s.length() - 1)) == false) {
-                    s = s.substring(0, s.length() - 1);
-                }
-
-                //check if word is filtered
-                if (allVegetables.contains(s)) {
-                    //TODO: Return result for message "violating" the filter.
-                } else {
-                    //TODO: Return whatever result for the message "passing" the filter.
-                }
-            }
-
-        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
